0d2811e246d5e3013b71a3771c4a67517b92b738
shiyanzhe <shi.yanzhe@alisports.com>
2018-07-19 20:27:05 +0800

    modify

--- auto_push.sh
+++ auto_push.sh
@@ -2,14 +2,14 @@
 
 echo "------------Begin------------"
 
-message = $1
+message=$1
 if [ ! -n "$1" ] ;then
 message="modify"
 fi
 
 git add .
 git commit -m $message
-echo $1
+echo $message
 git push
 
 echo "-------------End-------------"
